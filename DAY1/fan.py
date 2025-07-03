abdb2599a00741365511c8305a38891efe037a91
Gurunr1234 <nrguru1@gmail.com>
2025-07-03 16:24:41 +0530

    initialize

---
+++ DAY1/fan.py
@@ -0,0 +1,22 @@
+class Fan:
+    def __init__(self):
+        self.brand = "bajaj"
+        self.color = "black"
+        self.cost = 2000
+        self.noOfBlades = 3
+
+    def on(self):
+        print("the fan is on")
+
+    def rotate(self):
+        print("fan is rotating")
+
+f1 = Fan()
+
+print(f1.brand)
+print(f1.color)
+print(f1.cost)
+print(f1.noOfBlades)
+
+f1.on()
+f1.rotate()
