458740f7ed8358ca807d5d3caa099886b2e1def6
Gurunr1234 <nrguru1@gmail.com>
2025-07-04 21:02:47 +0530

    addition

--- DAY2/3addition.py
+++ DAY2/3addition.py
@@ -1,10 +1,10 @@
 # print("enter value1")
 # a = int(input())           OR
 
-a = int(input("enter value1: "))
+a, b = int(input("enter value1: ")), int(input("enter value2: "))
 
-print("enter value2")
-b = int(input())
+# print("enter value2")
+# b = int(input())
 
 c = a + b
 print("the sum is", c)
