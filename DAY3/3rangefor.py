7163fad20f378be2126afb136d226eff79596532
Gurunr1234 <nrguru1@gmail.com>
2025-07-05 13:01:58 +0530

    Range For loop

---
+++ DAY3/3rangefor.py
@@ -0,0 +1,12 @@
+#range(stop)
+
+for i in range(10):
+    print(i)
+
+#range(start, stop)
+for i in range(20, 30):
+    print(i)
+
+#range(start, stop, step)
+for i in range(11, 25, 2):
+    print(i)
