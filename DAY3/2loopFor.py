3e972aa75743c9b5672b61c797f0cf1d5702e187
Gurunr1234 <nrguru1@gmail.com>
2025-07-05 12:45:04 +0530

    for loop

---
+++ DAY3/2loopFor.py
@@ -0,0 +1,17 @@
+for i in 1, 2, 3, 4, 5:
+    print(i)
+
+
+
+for data in "rama", 23, 5.6, True:
+    print(data)
+
+
+
+
+for i in "python":
+    print(i)
+
+
+for i in "rama", "python", "java":
+    print(i)
