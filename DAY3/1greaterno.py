e0f10676778b226d2c80448b3d5cc9fee263477f
Gurunr1234 <nrguru1@gmail.com>
2025-07-05 12:14:27 +0530

    conditional (greater number)

---
+++ DAY3/1greaterno.py
@@ -0,0 +1,11 @@
+print("Enter a value1:")
+a = int(input())
+print("Enter a value2:")
+b = int(input())
+if (a > b):
+    print("a is greater than b")
+elif (a < b):
+    print("b is greater than a")
+else:
+    print("a is equal to b")
+    
