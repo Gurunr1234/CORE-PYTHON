1c216273300c8010f3b40930b38ec633433e4d95
Gurunr1234 <nrguru1@gmail.com>
2025-07-08 16:35:36 +0530

    method type 2

---
+++ DAY5/METHOD_tp_2.py
@@ -0,0 +1,18 @@
+class Demo:
+    def __init__(self):
+        self.a = 10
+        self.b = 20
+
+    def display(self):
+        x = 100
+        y = 200
+        z = x + y
+        return z
+
+d1 = Demo()
+
+print(d1.a)
+print(d1.b)
+
+r1 = d1.display()
+print(r1)
