11563120676a2427b3ce466dd528dfee3f3fbf03
Gurunr1234 <nrguru1@gmail.com>
2025-07-08 16:07:10 +0530

    bitwise operator

---
+++ DAY4/bitwiseOperator.py
@@ -0,0 +1,17 @@
+a = 5
+b = 4
+
+and_p = a & b
+print(and_p)
+
+or_p = a | b
+print(or_p)
+
+xor_p = a ^ b
+print(xor_p)
+
+left_shift = a << 1
+print(left_shift)
+
+right_shift = a >> 1
+print(right_shift)
