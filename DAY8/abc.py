6b72675aa3a811f75b621e5f746118c42d6c2433
Gurunr1234 <nrguru1@gmail.com>
2025-07-11 16:02:36 +0530

    types of instance variable declared

---
+++ DAY8/abc.py
@@ -0,0 +1,19 @@
+a = 10
+b = 10
+c = 10
+
+print(a)
+print(b)
+print(id(c))
+print(id(b))
+print(a is b)
+
+d = 20
+e = 20
+f = 20
+print(d)
+print(e)
+print(id(f))
+print(id(d))
+print(a is d)
+print(d is f)
