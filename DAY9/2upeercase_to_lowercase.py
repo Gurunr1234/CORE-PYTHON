d127853418694307ff5481717e179840ace36ff7
Gurunr1234 <nrguru1@gmail.com>
2025-07-15 15:52:12 +0530

    swap alphbet from upper to lower and reverse

---
+++ DAY9/2upeercase_to_lowercase.py
@@ -0,0 +1,21 @@
+print("enter a string:")
+str = input()
+
+i = 0
+newstr = ""
+while i < len(str):
+    a = str[i]
+    ascii = ord(a) # ord is used to convert character to ascii value
+    if (ascii >= 65 and ascii <= 90):  # check if the character is uppercase
+        ascii = ascii + 32
+        constr = chr(ascii)   # chr is used to convert ascii value to character 
+        newstr= newstr + constr
+    else:
+        # ascii = ascii - 32
+        # constr = chr(ascii)
+        # newstr = newstr + constr
+        newstr = newstr + a
+    
+    i = i + 1
+
+print("the new string is:", newstr)
