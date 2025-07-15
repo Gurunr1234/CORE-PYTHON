04916cb5a7130ba979d19c092cdaa1117bcaf940
Gurunr1234 <nrguru1@gmail.com>
2025-07-15 16:05:52 +0530

    strip inbuilt function

---
+++ DAY10/inbuilt_fun.py
@@ -0,0 +1,11 @@
+print("enter a string:")
+str = input()
+
+str1 = str.lower()
+print(str1)
+
+str2 = str.upper()
+print(str2) 
+
+str3 = str.swapcase()
+print(str3)
---
+++ DAY10/strip_method.py
@@ -0,0 +1,12 @@
+str = "              pentagon space          "
+
+print(str)
+
+str1 = str.lstrip()
+print(str1)
+
+str2 = str.rstrip()
+print(str2)
+
+str3 = str.strip()
+print(str3)
