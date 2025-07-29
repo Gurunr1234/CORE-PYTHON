2be22b76e954f287b00870412e1cfbdd560c9009
Gurunr1234 <nrguru1@gmail.com>
2025-07-29 16:28:25 +0530

    covert local to non local variable

---
+++ DAY16/convert_localTOnonlocal_var.py
@@ -0,0 +1,15 @@
+def fun1():
+    a = 999
+    print("from fun1 a is ", a)
+    def fun2():
+        nonlocal a
+        a = 10
+        b = 20
+        print("from fun2 b is ", b)
+        print("from fun2 a is ", a)
+
+    fun2()
+    print("from fun1 a is ", a)
+    #print("from fun1 b is ", b)
+
+fun1()
